5b07368b7708e3ae2ed010c2ccb04e31de97da31
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-17 23:49:19 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Denis Bokov on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
